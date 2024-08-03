77387823a0b6b5decab1b6f8560be0cd1cced21e
Dhanasekaran M <149887366+Dhanasekaran-MS@users.noreply.github.com>
2024-08-03 13:37:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,15 +16,20 @@ uploaded_file = st.file_uploader("Upload an image of a digit (0-9)", type=["jpg"
 if uploaded_file is not None:
     # Load and process the image
     image = Image.open(uploaded_file).convert('L')  # Convert to grayscale
-    image = image.resize((28, 28))  # Resize to 28x28 pixels
-    image = np.array(image)  # Convert to numpy array
-    image = image / 255.0  # Normalize to [0, 1] range
-    image = image.reshape(1, 28, 28, 1)  # Add batch dimension
+    img = image.resize((28, 28))  # Resize to 28x28 pixels
+    img = np.array(img)  # Convert to numpy array
+    img = img / 255.0  # Normalize to [0, 1] range
+    img = img.reshape(1, 28, 28)  # Add batch dimension
 
     # Make predictions
-    prediction = model.predict(image)
-    predicted_digit = np.argmax(prediction)
+    prediction = model.predict(img)
+    digit = np.argmax(prediction)
 
+    st.balloons()
     # Display the results
     st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
+    st.write('-------------------------------------------------------------')
+    st.write('-------------------------------------------------------------')
     st.write("Predicted Digit:", predicted_digit)
+
+
