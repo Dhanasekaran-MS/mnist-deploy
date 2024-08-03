427c8c6527dd230c7a2e670abe024c4c165076bb
Dhanasekaran M <149887366+Dhanasekaran-MS@users.noreply.github.com>
2024-08-03 13:51:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ model = tf.keras.models.load_model('mnist_model.h5')
 
 # Define the web app layout
 st.title(":red[MNIST Digit Recognizer]")
+st.write('[click to get Sample Images of digits](https://www.google.com/imgres?q=mnist%20images&imgurl=https%3A%2F%2Fwww.researchgate.net%2Fpublication%2F321174607%2Ffigure%2Ffig3%2FAS%3A806993333850113%401569413612260%2FExample-of-a-MNIST-input-An-image-is-passed-to-the-network-as-a-matrix-of-28-by-28.png&imgrefurl=https%3A%2F%2Fwww.researchgate.net%2Ffigure%2FExample-of-a-MNIST-input-An-image-is-passed-to-the-network-as-a-matrix-of-28-by-28_fig3_321174607&docid=SscvN6rlLtzgyM&tbnid=yEDu2ks1PlxS8M&vet=12ahUKEwip7-OfsdiHAxUwyDgGHRYECmAQM3oECCoQAA..i&w=176&h=176&hcb=2&ved=2ahUKEwip7-OfsdiHAxUwyDgGHRYECmAQM3oECCoQAA)')
 st.header("Upload Image")
 
 uploaded_file = st.file_uploader("Upload an image of a digit (0-9)", type=["jpg", "png", 'webp'])
